0066920d00055a3010308cd1ac5d5dbf1026411a
LenaTarashchik <58592485+LenaTarashchik@users.noreply.github.com>
2019-12-06 17:51:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Пользователь вводит два числа. Вывести на экран большее
+            Console.WriteLine("Введите первое число:");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            int second = Convert.ToInt32(Console.ReadLine());
+            if (first > second)
+            {
+                Console.WriteLine("Большее:" + first);
+            }
+            else Console.WriteLine("Большее:" + second); 
+            Console.ReadLine();
+
+  /*Пользователь вводит слово «мяу» или слово «гав» если слово
+введенное пользователем «мяу» вывести на экран «Покорми кота» иначе «Погуляй с собакой»*/
+            
+            Console.WriteLine("Введите мяу или гав");
+            string s = Console.ReadLine();
+            if (s == "мяу")
+            {
+                Console.WriteLine("Покорми кота");
+            }
+            else if (s == "гав")
+                Console.WriteLine("Погуляй с собакой");
+            Console.ReadLine();
+        }
+    }
+}

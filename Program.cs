a0d4e2ddb64a6d6e7b28ca29447095b7db40825b
LenaTarashchik <58592485+LenaTarashchik@users.noreply.github.com>
2019-12-06 17:54:29 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,47 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Task3
+namespace Task1
 {
     class Program
     {
         static void Main(string[] args)
         {
-//            Пользователь вводит номер месяца если месяц 1,2,12 вывести на экран
-//«Зима»; 3,4,5 – «Весна»; 6-8 – «Лето»; 9-11 – «Осень». В любом другом
-//случае «На этой планете такого месяца нет»:
-            Console.WriteLine("Введите номер месяца(1-12):");
-            int number = Convert.ToInt32(Console.ReadLine());
-            switch(number)
+            //Пользователь вводит два числа. Вывести на экран большее
+            Console.WriteLine("Введите первое число:");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            int second = Convert.ToInt32(Console.ReadLine());
+            if (first > second)
             {
-                case 1: Console.WriteLine("Зима");
-                    break;
-                case 2: Console.WriteLine("Зима");
-                    break;
-                case 3: Console.WriteLine("Весна");
-                    break;
-                case 4: Console.WriteLine("Весна");
-                    break;
-                case 5: Console.WriteLine("Весна");
-                    break;
-                case 6: Console.WriteLine("Лето");
-                    break;
-                case 7: Console.WriteLine("Лето");
-                    break;
-                case 8: Console.WriteLine("Лето");
-                    break;
-                case 9: Console.WriteLine("Осень");
-                    break;
-                case 10: Console.WriteLine("Осень");
-                    break;
-                case 11: Console.WriteLine("Осень");
-                    break;
-                case 12: Console.WriteLine("Зима");
-                    break;
-                default: 
-                    Console.WriteLine("На этой планете такого месяца нет");
-                    break;
+                Console.WriteLine("Большее:" + first);
             }
+            else Console.WriteLine("Большее:" + second); 
+            Console.ReadLine();
+
+  /*Пользователь вводит слово «мяу» или слово «гав» если слово
+введенное пользователем «мяу» вывести на экран «Покорми кота» иначе «Погуляй с собакой»*/
+            
+            Console.WriteLine("Введите мяу или гав");
+            string s = Console.ReadLine();
+            if (s == "мяу")
+            {
+                Console.WriteLine("Покорми кота");
+            }
+            else if (s == "гав")
+                Console.WriteLine("Погуляй с собакой");
             Console.ReadLine();
         }
     }
